1a607fad3c7abbfb01f1c03aad3fff53251fa6b7
cheyennerossler-demskie <cheyennerossler@gmail.com>
2025-04-10 12:09:34 -0400

    moved node modules folder back out of 'public' folder

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // entry point, see package.json
+// specify we want to use express
+const express = require('express')
+const app = express()
+
+//const userRoutes = require("./server/routes/user")
+// do same for all entities
+
+//app.use("/users", userRoutes)
+// do same for all entities
+
+const PORT = process.env.PORT || 3000; // mySQL benchmark - computer not using
+app.listen(PORT, () => console.log(`Server started on port ${PORT}!`));
