e1907be3114234c469a517012b51f976bd5cf3f6
cheyennerossler-demskie <cheyennerossler@gmail.com>
2025-04-14 17:22:50 -0400

    Re-organized folders/files. Updated 3 input forms,, created JavaScript objects for submitted forms, and used addEventListener to obtain submitted variables

---
+++ public/login.js
@@ -0,0 +1,35 @@
+let loginForm = document.getElementById('loginForm')
+loginForm.addEventListener('submit', login)
+
+function login(e){
+    e.preventDefault()
+    let errorSection = document.getElementById("error")
+
+    let username = document.getElementById('username').value
+    let password = document.getElementById('password').value
+
+    if(validString(username)) {
+        errorSection.innerText = `Username cannot be blank!!!`
+    } 
+    else {
+        errorSection.innerText = ""  
+        console.log(username)
+    }
+
+    const user = {
+        username: username,
+        password: password
+    }
+
+    let section = document.getElementById("welcome")
+    section.innerHTML = `Welcome, ${username}!`
+
+    console.log(user)
+}
+
+document.getElementById('username').value = ""
+document.getElementById('password').value = ""
+
+function validString(word) {
+    return word == ""
+}
